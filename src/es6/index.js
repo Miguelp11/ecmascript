813f58177abcf5ce38c59a39f3296e8bfde8b53b
Miguel Ponce Local <miguel_p1194@hotmail.com>
2021-08-15 18:04:03 -0500

    el archivo index.js se agregan lineas de codigo explicando las diferencias ES6 en multilineas, destructuracion, spread operator y var, let y const

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,4 +1,4 @@
-// Parametros por defecto antes de ES6
+// Parametros por defecto 
 function nuevaFuncion(nombre, edad, localidad){
     var nombre = nombre || "Miguel";
     var edad = edad || 24;
@@ -14,7 +14,7 @@ function nuevaFuncion2(nombre="Miguel", edad=24, localidad="MX"){
 nuevaFuncion2();
 nuevaFuncion2("cansino",12,"CH");
 
-// Concatenacion antes de ES6
+// Concatenacion
 let hola = "Hola";
 let mundo = "Mundo";
 let fraseEpica= hola+" "+mundo;
@@ -23,3 +23,52 @@ console.log(fraseEpica);
 // Templated literals o template strings ES6
 let fraseEpica2= `${hola} ${mundo}`;
 console.log(fraseEpica2);
+
+// Multilineas 
+let lorem = "lorem asda ererasd asdasdasd asdasdas \n"
++ "otra frase epica";
+// Multilineas en ES6
+let lorem2 = `esto es una frase epica
+Esto es una nueva frase eqpica`;
+
+console.log(lorem);
+console.log(lorem2);
+
+// Destructuracion 
+let persona = {
+    nombre: "miguel",
+    edad: 21,
+    pais: "MX"
+}
+console.log(persona.nombre,  persona.edad, persona.pais);
+
+// Destructuracion ES6
+let {nombre, pais} = persona;
+console.log(nombre, pais);
+
+// Arreglos
+let equipo1 = ["oscar", "ricardo", "juan"];
+let equipo2 = ["valeria", "jessica", "camila"];
+
+let educacion = ["David", "oscar", "ricardo", "juan","valeria", "jessica", "camila"]
+// Spread operator ES6
+let educacion1 = ["David", ...equipo1, ...equipo2];
+console.log(educacion1);
+
+// Variables con var, que su scope es de forma global
+{
+    var globalVar = "variable global";
+}
+
+{
+    // Let su scope es de bloque o de funcion ES6
+    let globalLet = "variable let global";
+    console.log(globalLet);
+}
+
+console.log(globalVar);
+
+// const almacena un valor constante y no puede ser reasignado ES6
+const a = "b";
+a = "a";
+console.log(a);
