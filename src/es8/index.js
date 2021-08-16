d1f95ab760c467a82b51d896e343a00621cf083d
Miguel Ponce Local <miguel_p1194@hotmail.com>
2021-08-16 18:41:32 -0500

    Se agrega la carpeta es8 y el archivo index.js explicando algunas integraciones en esta version de ES8

---
+++ src/es8/index.js
@@ -0,0 +1,33 @@
+// ES8 Junio del 2017
+// Object.entries devuelve las propiedades [clave y valor] en una matriz
+
+const data = {
+    front: "oscar",
+    back: "juan",
+    diseno: "ana"
+}
+
+const entries = Object.entries(data);
+console.log(entries);
+console.log(entries.length);
+
+// object.values devuelve los valores de un objeto a un arreglo
+const data1 = {
+    front: "oscar",
+    back: "juan",
+    diseno: "ana"
+}
+
+const values = Object.values(data1);
+console.log(values);
+console.log(values.length);
+
+// padding agrega texto al principio o al final de un string
+let string = "hola";
+console.log(string.padStart(7, "Hi"));
+console.log(string.padEnd(12, ' ------'));
+
+// Trailing-comas podemos poner una coma al final que establece que al siguiente puede haber un valor o no y no lo marca como un error, por lo que no pasa nada
+const obj ={
+    nombre: "miguel",
+}
